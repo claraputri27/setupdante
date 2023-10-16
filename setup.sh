071d1aff82c31d50c16c8870c440abfe0b343630
Dgthny <108542831+Dgthny@users.noreply.github.com>
2023-10-16 11:52:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 # Perbarui indeks paket
 sudo apt update
 # Instal paket-paket yang diinginkan
-sudo apt install -y ufw git dante-server
+sudo apt install -y ufw dante-server
 # Tampilkan pesan selesai
+sudo mv setupdante/danted.conf /etc
+sudo systemctl restart danted.service
 echo "Instalasi selesai"
