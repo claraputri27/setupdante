b465b11f22af561778bceb09c1631759000775e6
Dgthny <108542831+Dgthny@users.noreply.github.com>
2023-10-16 11:49:54 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Perbarui indeks paket
+sudo apt update
+# Instal paket-paket yang diinginkan
+sudo apt install -y ufw git dante-server
+# Tampilkan pesan selesai
+echo "Instalasi selesai"
