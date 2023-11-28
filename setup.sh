a05cc50f197eeda8007b03eb47c97447fe18430c
claraputri27 <151874771+claraputri27@users.noreply.github.com>
2023-11-28 12:50:57 -0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 useradd -r -s /bin/false empo
-sudo mv setupdante/danted.conf /etc
-sudo systemctl restart danted.service
+mv setupdante/danted.conf /etc
+systemctl restart danted.service
 echo "Instalasi selesai"
-sudo passwd empo
+passwd empo
