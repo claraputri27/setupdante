c88db90eee6ee527adfd1c3cfea1d53c1f6bfecb
claraputri27 <151874771+claraputri27@users.noreply.github.com>
2023-11-28 13:33:51 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt install dante-server -y
 useradd -r -s /bin/false empo
-mv setupdante/danted.conf /etc
+mv danted.conf /etc
 systemctl restart danted.service
 echo "Instalasi selesai"
 sudo passwd empo
